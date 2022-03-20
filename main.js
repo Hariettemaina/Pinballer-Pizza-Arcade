e6019326238b80e34755c213e710bfaaf326b801
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-20 23:33:17 +0300

    added constructor

--- main.js
+++ main.js
@@ -8,3 +8,16 @@ $(document).ready(function(){
 
 })
 
+class Pizza {
+    constructor(name,size,sizePrice,crust,crustPrice, toppings,toppingsPrice,delivery,deliveryFee,quantity) {
+      this.name = name  
+      this.size = size;
+      this.sizePrice = sizePrice
+      this.crust = crust;
+      this.crustPrice = crustPrice
+      this.toppings = toppings;
+      this.toppingsPrice = toppingsPrice
+      this.delivery = delivery;
+      this.deliveryFee = deliveryFee
+      this.quantity = quantity;
+    }
