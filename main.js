c083fa635208e764390046e5c7986478dc70643d
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-03-20 23:40:16 +0300

    added function

--- main.js
+++ main.js
@@ -21,3 +21,30 @@ class Pizza {
       this.deliveryFee = deliveryFee
       this.quantity = quantity;
     }
+}
+let pizzaOrders =[]
+let currentPizzaName = null;
+let currentPizzasPrice = 0;
+let currentCrust ='';
+let currentCrustPrice =0;
+let currentSizePrice =5;
+let currentSize=''
+let currentPizzaToppings=[]
+let currentToppingsPrice =0
+let pizzaQuantity =1
+let deliveryOption = false
+let deliveryOptedInFee =0
+let pickPizzaAction = (pizzaName) => {
+    currentPizzaName = pizzaName
+    pizzaQuantity=1
+    $(document).ready(function(){
+        $('#toppings').show()
+        $('#pizzaList').hide()
+        $('#welcomemessage').hide()
+        $('#customizemessage').show()
+        $("#price").val(currentPizzasPrice);
+        $('#deliveryNote').hide()
+        
+    })
+    
+}
